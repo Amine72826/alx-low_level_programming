c02f62eb5f270489f6fc3bf535d54641edeeb09f
Amine <Amine.bahlas@hotmail.com>
2023-02-24 01:20:10 +0100

    hot fix and update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,18 @@
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
 	if (n <= 0)
 	{
-		_putchar('\');
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
