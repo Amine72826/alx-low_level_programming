32128da538498d9e5fc97bfdd928709dd04e3a7c
Amine <Amine.bahlas@hotmail.com>
2023-02-24 01:11:52 +0100

    hot fix and update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,4 +15,8 @@ void print_line(int n)
 		_putchar('_');
 	}
 	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\');
+	}
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - print diagonal line
+ * @n: number of times to use \
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int spc;
+
+	for (i = 0; i < n; i++)
+	{
+		for (spc = 0; spc < 1; spc++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
