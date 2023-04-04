12da52a248e680b0805ddd6e8899935da0685668
Amine <Amine.bahlas@hotmail.com>
2023-04-04 18:23:24 +0000

    Hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,26 +2,52 @@
 #include <stdlib.h>
 
 /**
- * get_nodeint_at_index - Find a node in a list.
+ * insert_nodeint_at_index - Find a node in a list.
  * @head: Address of the first node in a list.
- * @index: Position of a the node to find (starting from 0).
- * Return: Node address.
+ * @idx: Position of a the node to find (starting from 0).
+ * @n : the value to be stored in the new node.
+ * Return: Node address or NULL
  **/
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	if (head == NULL)
 		return (NULL);
 
-	unsigned int count = 0;
-	listint_t *current = head;
+	unsigned int i = 0;
+	listint_t *current = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-	while (current != NULL && count < index)
-	{
-		current = current->next;
-		count++;
-	}
-	if (count < index || current == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-	return (current);
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (i < idx - 1)
+	{
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL || current->next == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
 }
