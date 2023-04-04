f15197932a62347d5ef80957c24c533d4f4bb39e
Amine <Amine.bahlas@hotmail.com>
2023-04-04 18:06:48 +0000

    Hot fix on get_node and insert_node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,24 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
- * Return: pointer to the new node, or NULL
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+ * get_nodeint_at_index - Find a node in a list.
+ * @head: Address of the first node in a list.
+ * @index: Position of a the node to find (starting from 0).
+ * Return: Node address.
+ **/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if (head == NULL)
-		return (NULL);
-
-	listint_t *new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	listint_t *prev_node = NULL;
-	listint_t *curr_node = *head;
 	unsigned int i = 0;
 
-	while (i < idx && curr_node != NULL)
-	{
-		prev_node = curr_node;
-		curr_node = curr_node->next;
-		i++;
-	}
-
-	if (i < idx)
+	if (head == NULL)
 		return (NULL);
-	prev_node->next = new_node;
-	new_node->next = curr_node;
-	return (new_node);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
 }
