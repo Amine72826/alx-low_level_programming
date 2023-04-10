8f0321584f43e3a16af2551e9b1e4fe14d4d02be
Amine <Amine.bahlas@hotmail.com>
2023-04-10 18:43:36 +0000

    Fix on get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value = (n >> index) & 1;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-	return (bit_value);
+	return ((n >> index) & 1);
 }
