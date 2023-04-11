9bf90b851d92c483cc46c7bc4627bc6126898273
Amine <Amine.bahlas@hotmail.com>
2023-04-11 02:26:24 +0000

    fix on read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
@@ -8,30 +10,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
-	char *buf;
+	int a, b;
+	char *buff;
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	a = read(fd, buff, letters);
+	if (a < 0)
 	{
-		free(buf);
+		free(buff);
 		return (0);
 	}
-	buf[i] = '\0';
+	buff[a] = '\0';
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+	b = write(STDOUT_FILENO, buff, a);
+	if (b < 0)
 	{
-		free(buf);
+		free(buff);
 		return (0);
 	}
-	free(buf);
-	return (y);
+	free(buff);
+	return (b);
 }
