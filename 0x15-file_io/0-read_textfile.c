70ad15db287f6efada4d34887e4dce31387e27cd
Amine <Amine.bahlas@hotmail.com>
2023-04-11 01:57:31 +0000

    fix on read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,28 +6,47 @@
  * @letters: the number of letters to read and print
  * Return: the number of letters it could read and print
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, n_bytes;
-	char buf[1024];
+	int fd, nread;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
 		return (0);
 
-	while ((n_bytes = read(fd, buf, 1024)) > 0 && letters > 0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		if (write(STDOUT_FILENO, buf, n_bytes) != n_bytes)
-		{
-			close(fd);
-			return (0);
-		}
-		letters -= n_bytes;
+		free(buf);
+		return (0);
 	}
 
+	nread = read(fd, buf, letters);
+	if (nread == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	buf[nread] = '\0';
+	if (write(STDOUT_FILENO, buf, nread) != nread)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
 	close(fd);
 
-	return (n_bytes == -1 ? 0 : n_bytes);
+	return (nread);
 }
